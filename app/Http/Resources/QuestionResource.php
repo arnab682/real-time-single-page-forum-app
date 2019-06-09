ff4d6ee3c2c9ee69f509a747adbd99ff4b129333
arnab682 <arnab.das682@gmail.com>
2019-06-10 02:39:25 +0600

    CRUD for Question along with Question Resource Transformation

--- app/Http/Resources/QuestionResource.php
+++ app/Http/Resources/QuestionResource.php
@@ -14,6 +14,14 @@ class QuestionResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+        return[
+
+            'title' => $this->title,
+            'path' => $this->path,
+            'body' => $this->body,
+            'created_at' => $this->created_at->diffForHumans(),
+            'user' => $this->user->name
+        ];
     }
 }
